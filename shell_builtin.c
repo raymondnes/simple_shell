f1bf1dd078cb6dffc8c9e7b1a3111a8d6f5e0c1c
COBIECODES <dibia3527@gmail.com>
2023-02-22 18:58:56 -0800

    task update

--- shell_builtin.c
+++ shell_builtin.c
@@ -56,4 +56,128 @@ int shell_setenv(char **args)
 		;
 	if (args_len != 3)
 	{
+		errno = EINVAL;
+		perror("Usage: setenv [variable] [value]\n");
+		return (-1);
+	}
+	if (name == NULL || value == NULL)
+	{
+		errno = EINVAL;
+		perror(NULL);
+		return (-1);
+	}
+	if (getenv(name)) /*if the variable exists in environ, unset it */
+		_unsetenv(name);
+	new_entry = malloc(strlen(name) + strlen(value) + 2);
+	if (!new_entry)
+	{
+		perror(NULL);
+		return (-1);
+	}
+	strcpy(new_entry, name);
+	strcat(new_entry, "=");
+	strcat(new_entry, value);
+	ret = putenv(new_entry);
+	if (ret)
+	{
+		perror(NULL);
+		return (-1);
+	}
+	return (0);
+}
+/**
+* shell_env - prints the environment
+* @args: array of strings
+* Description: uses the environ var
+* Return: 1 if successful
+*/
+int shell_env(__attribute__((unused)) char **args)
+{
+	int i = 0;
 
+	for (i = 0; environ[i]; i++)
+		printf("%s\n", environ[i]);
+	return (1);
+}
+
+
+/**
+* shell_cd - change directory
+* @args: array of strings
+*
+* Description: takes a pathname and calls chdir
+* Return: 1 if successful
+*/
+
+int shell_cd(char **args)
+{
+	char *curr_dir;
+	int args_len;
+
+	for (args_len = 0; args[args_len]; args_len++)
+		;
+	if (args_len != 2 && args_len != 1)
+	{
+		errno = EINVAL;
+		perror("Usage: cd [Directory]\n");
+		return (-1);
+	}
+	curr_dir = malloc(sizeof(char) * 1024);
+	if (!curr_dir)
+	{
+		perror(NULL);
+		return (-1);
+	}
+	if (args_len == 1)
+	{
+		go_to_home_dir(curr_dir);
+		free(curr_dir);
+		return (0);
+	}
+	if (strcmp(args[1], "-") == 0)
+	{
+		go_to_prev_dir(curr_dir);
+		free(curr_dir);
+		return (0);
+	}
+
+	else
+	{
+		go_to_named_dir(curr_dir, args[1]);
+		free(curr_dir);
+		return (0);
+	}
+	return (0);
+}
+
+
+/**
+* shell_exit - exits the shell program
+* @args: array of strings
+* Description: simply calls exit()
+* Return: exits with 0
+*/
+
+int shell_exit(char **args)
+{
+	int status, args_len;
+
+	for (args_len = 0; args[args_len]; args_len++)
+		;
+	if (args_len != 1 && args_len != 2)
+	{
+		errno = EINVAL;
+		perror("Usage: exit or exit [integer]\n");
+		return (6999);
+	}
+
+	if (args_len == 1)
+		return (6990);
+
+	status = atoi(args[1]);
+
+	if (args_len == 2)
+		return (status);
+	return (0);
+
+}
